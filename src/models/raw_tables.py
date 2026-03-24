587a977a5d6972ee20f6cfeed43121ce5887ab91
Juan Cruz Pappolla <jcp12196@gmail.com>
2026-03-24 18:18:26 -0300

    Added models and requirements

---
+++ src/models/raw_tables.py
@@ -0,0 +1,49 @@
+from datetime import datetime
+from sqlalchemy import (
+        BigInteger,
+        String,
+        Text,
+        DateTime,
+        ForeignKey
+)
+from sqlalchemy.orm import (
+    DeclarativeBase,
+    Mapped,
+    mapped_column,
+    relationship
+)
+from sqlalchemy.dialects.postgresql import JSONB
+
+class Base(DeclarativeBase):
+    pass
+
+class RawPages(Base):
+    __tablename__ = "raw_pages"
+
+    scrape_id: Mapped[int] = mapped_column(
+            BigInteger,
+            primary_key=True
+    )
+    store: Mapped[str] = mapped_column(String(255))
+    url: Mapped[str] = mapped_column(String(255))
+    html: Mapped[str] = mapped_column(Text)
+    time: Mapped[datetime] = mapped_column(DateTime(timezone=True))
+
+    products: Mapped["RawProducts"] = relationship(back_populates="raw_pages")
+
+    def __repr__(self) -> str:
+        return f"RawPages(scrape_id={self.scrape_id}, url={self.url})"
+
+class RawProducts(Base):
+    __tablename__ = "raw_products"
+
+    scrape_id: Mapped[int] = mapped_column(
+            BigInteger,
+            ForeignKey("raw_pages.scrape_id")
+    )
+    payload: Mapped[dict] = mapped_column(JSONB)
+
+    pages: Mapped[RawPages] = relationship(back_populates="raw_products")
+
+    def __repr__(self) -> str:
+        return f"RawProducts(scrape_id={self.scrape_id})"
