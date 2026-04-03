bb4c2f44c2af4bc845d9865bff54e5831f4b7f21
Juan Cruz Pappolla <jcp12196@gmail.com>
2026-04-03 17:02:16 -0300

    Updated requirements

---
+++ src/main.py
@@ -0,0 +1,37 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from src.scrapers.coto_scraper import CotoScraper
+from src.scrapers.carrefour_scraper import CarrefourScraper
+from src.utils.logging import get_logger
+logger = get_logger("main")
+
+def main():
+    logger.info("Initializing scraping and parsing scripts...")
+
+    success = load_dotenv()
+    if not success:
+        logger.error("An error ocurred loading .env file")
+        raise RuntimeError
+
+    db_url = os.getenv("DATABASE_URL")
+    if db_url is None:
+        logger.error("An eror ocurred loading database url from .env file")
+        raise RuntimeError
+
+    engine = create_engine(db_url)
+    session = sessionmaker(bind=engine)
+
+    scrapers_by_store = {
+            "coto": CotoScraper(),
+            "carrefour": CarrefourScraper()
+    }
+    for store, scraper in scrapers_by_store:
+        try:
+            scraper.scrape(session)
+        except Exception as e:
+            logger.exception(f"An error ocurred scraping store {store}")
+    
+    
