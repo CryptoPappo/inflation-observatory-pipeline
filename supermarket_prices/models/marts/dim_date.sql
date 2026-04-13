0b8e94178998d111b3732bb8083b8711a896aad7
Juan Cruz Pappolla <jcp12196@gmail.com>
2026-04-13 19:34:43 -0300

    Finished adding holidays

--- supermarket_prices/models/marts/dim_date.sql
+++ supermarket_prices/models/marts/dim_date.sql
@@ -24,6 +24,7 @@ full_dates as (
 
 	select 
 		d.*,
+		to_char(d.date_day, 'MM-DD') as month_day
 		f.fiscal_week_of_year,
 	    	f.fiscal_week_of_period,
 	    	f.fiscal_period_number,
@@ -105,6 +106,72 @@ step7 as (
 		make_date(year_number, 03, 22) + (d + e - o) as easter_date
 
 	from step6
-)
+),
 
-select * from step7
+full_holidays as (
+
+	select
+		row_number() over (order by date_day) as date_key,
+		date_day,
+		month_day,
+		prior_date_day,
+		next_date_day,
+		prior_year_date_day,
+		prior_year_over_year_date_day,
+		day_of_week,
+		day_of_week_name,
+		day_of_week_name_short,
+		day_of_month,
+		day_of_year,
+		week_start_date,
+		prior_year_week_start_date,
+		prior_year_week_end_date,
+		week_of_year,
+		iso_week_start_date,
+		iso_week_end_date,
+		prior_year_iso_week_start_date,
+		prior_year_iso_week_end_date,
+		iso_week_of_year,
+		prior_year_week_of_year,
+		prior_year_iso_week_of_year,
+		month_of_year,
+		month_name,
+		month_name_short,
+		month_start_date,
+		month_end_date,
+		prior_year_month_start_date,
+		prior_year_month_end_date,
+		quarter_of_year,
+		quarter_start_date,
+		quarter_end_date,
+		year_number,
+		year_start_date,
+		year_end_date,
+		fiscal_week_of_year,
+		fiscal_week_of_period,
+		fiscal_period_number,
+		fiscal_quarter_number,
+		fiscal_period_of_quarter,
+		case
+			when month_day = '01-01' then 'Año Nuevo'
+			when date_day = easter_date - interval '48 day' then 'Primer día de Carnaval'
+			when date_day = easter_date - interval '47 day' then 'Segundo día de Carnaval'
+			when month_day = '03-24' then 'Memoria por la Verdad y la Justicia'
+			when month_day = '04-02' then 'Día del Veterano'
+			when date_day = easter_date - interval '3 day' then 'Jueves Santo'
+			when date_day = easter_date - interval '2 day' then 'Viernes Santo'
+			when month_day = '05-01' then 'Día del Trabajador'
+			when month_day = '05-25' then 'Día de la Revolución de Mayo'
+			when month_day = '06-17' then 'Inmortalidad Gral. Guemes'
+			when month_day = '06-20' then 'Inmortalidad Gral. Belgrano'
+			when month_day = '07-09' then 'Día de la Independencia'
+			when month_day = '08-17' then 'Inmortalidad Gral. San Martín'
+			when month_day = '10-12' then 'Día de la raza'
+			when month_day = '11-23' then 'Día de la Soberanía Nacional'
+			when month_day = '12-08' then 'Inmaculada Concepción de María'
+			when month_day = '12-25' then 'Navidad'
+			else null
+		end as holiday_name
+),
+
+select * from full_holidays
