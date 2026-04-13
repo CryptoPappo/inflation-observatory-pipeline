b5a79cd2556283d14ff758e4ab57505d24195444
CryptoPappo <cryptopappo@protonmail.com>
2026-04-13 21:51:10 +0200

    Fixed date dimension error

--- supermarket_prices/models/marts/date_dimension.sql
+++ supermarket_prices/models/marts/date_dimension.sql
@@ -11,7 +11,7 @@ dates_raw as (
 	{{ dbt_utils.date_spine(
 		datepart="day",
 		start_date="cast('2000-01-01' as date)",
-		end_date="date_add(current_date(), interval 20 year)"
+		end_date="current_date + interval '20 year'"
 		)
 	}}
 
