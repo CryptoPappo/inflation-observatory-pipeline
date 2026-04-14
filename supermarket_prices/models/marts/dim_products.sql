891707601a5a2f715cad1423e6251ed0d780a13e
Juan Cruz Pappolla <jcp12196@gmail.com>
2026-04-14 18:07:33 -0300

    Finished products dimension table

--- supermarket_prices/models/marts/dim_products.sql
+++ supermarket_prices/models/marts/dim_products.sql
@@ -4,6 +4,8 @@
 	)
 }}
 
+-- We use a SCD Type 1 to describe the products dimension table
+
 with
 
 ranked_products as (
@@ -13,4 +15,21 @@ ranked_products as (
 		product_name,
 		category,
 		subcategory,
+		brand,
+		scraped_at,
+		row_number() over (partition by product_id order by scraped_at desc) as rn
 
+	from {{ ref('int_products') }}
+
+)
+
+select 
+	product_id,
+	product_name,
+	category,
+	subcategory,
+	brand,
+	scraped_at
+
+from ranked_products
+where rn = 1
