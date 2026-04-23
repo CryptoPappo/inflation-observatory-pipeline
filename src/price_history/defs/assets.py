c7af35987980d41abc75ba90a6884ba23a4a4cd8
Juan Cruz Pappolla <jcp12196@gmail.com>
2026-04-23 19:15:41 -0300

    Added dbt as a resource

--- src/price_history/defs/assets.py
+++ src/price_history/defs/assets.py
@@ -1,10 +1,12 @@
 import dagster as dg
 from sqlalchemy.orm import sessionmaker
+from dagster_dbt import DbtCliResource, dbt_assets
 
 from price_history.scrapers.base import BaseScraper
 from price_history.scrapers.coto_scraper import CotoScraper
 from price_history.scrapers.carrefour_scraper import CarrefourScraper
 from price_history.loaders.load_raw_data import load_raw_responses, load_normalized_responses
+from .resources import dbt_project
 
 def make_raw_asset(scraper_cls: BaseScraper) -> dg.Definitions:
     asset_key = f"scraper_{scraper_cls.store}"
@@ -74,3 +76,7 @@ def defs():
     total_assets = raw_assets + normalized_assets
 
     return dg.Definitions.merge(*total_assets)   
+
+@dbt_assets(manifest=dbt_project.manifest_path)
+def dbt_models(context: dg.AssetExecutionContext, dbt: DbtCliResource):
+    yield from dbt.cli(["build"], context=context).stream()
--- src/price_history/defs/resources.py
+++ src/price_history/defs/resources.py
@@ -1,16 +1,24 @@
 import os
 import dagster as dg
+from pathlib import Path
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from dagster_dbt import DbtCliResource, DbtProject
 
 load_dotenv()
 engine = create_engine(os.getenv("DATABASE_URL"))
 
+dbt_project_directory = Path(__file__).absolute().parent.parent.parent.parent / "supermarket_prices"
+dbt_project = DbtProject(project_dir=dbt_project_directory)
+
+dbt_resource = DbtCliResource(project_dir=dbt_project)
+
 @dg.definitions
-def resources():    
+def resources():
     return dg.Definitions(
-            resources={
-                "postgres_session": sessionmaker(bind=engine)
-            },
+        resources={
+            "dbt": dbt_resource,
+            "postgres_session": sessionmaker(bind=engine)
+        }
     )
