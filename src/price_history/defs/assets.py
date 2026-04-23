da196858429a8578e4c52754b463be21a3c61aa8
Juan Cruz Pappolla <jcp12196@gmail.com>
2026-04-23 17:30:12 -0300

    Created raw and normalized assets

--- src/price_history/defs/assets.py
+++ src/price_history/defs/assets.py
@@ -1,15 +1,14 @@
 import dagster as dg
 
-from price_history.scrapers.base import BaseScraper
 from price_history.scrapers.coto_scraper import CotoScraper
 from price_history.scrapers.carrefour_scraper import CarrefourScraper
 from price_history.loaders.load_raw_data import load_raw_responses, load_normalized_responses
 
-def make_raw_asset(scraper_cls: BaseScraper):
+def make_raw_asset(scraper_cls):
     asset_key = f"scraper_{scraper_cls.store}"
 
     @dg.asset(name=asset_key)
-    def _raw(context: dg.AssetExecutionContext):
+    def _raw(context):
         scraper = scraper_cls(scraper_id=context.run.run_id)
         raw_responses = scraper.scrape()
 
@@ -20,4 +19,46 @@ def make_raw_asset(scraper_cls: BaseScraper):
 
     return _raw
 
+def make_normalized_asset(scraper_cls):
+    asset_key = f"parser_{scraper_cls.store}"
 
+    @dg.asset(
+            name=asset_key,
+            ins={
+                "raw_responses": dg.AssetIn(key=f"scraper_{scraper_cls.store}")
+            }
+    )
+    def _normalized(context, raw_responses):
+        scraper = scraper_cls(scraper_id=context.run.run_id)
+
+        normalized_responses = []
+        for raw_response in raw_responses:
+            if raw_response["response_category"] != "product" or\
+                    raw_response["response_type"] != "json":
+                continue
+            try:
+                normalized_responses.append(
+                        {
+                            "raw_id": raw_response["raw_id"],
+                            "normalized_payload": scraper.parse(raw_response["payload"])
+                        }
+                )
+            except Exception as e:
+                #logger.exception(f"Failed parsing response: {raw_response}")
+                continue
+
+        Session = context.postgres_sessionmaker
+        load_normalized_responses(normalized_responses, Session)
+
+    return _normalized
+
+
+scrapers_cls = [
+        CotoScraper,
+        CarrefourScraper
+]
+raw = []
+normalized = []
+for scraper_cls in scrapers_cls:
+    raw.append(make_raw_asset(scraper_cls))
+    normalized.append(make_normalized_asset(scraper_cls))
